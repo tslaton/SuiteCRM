c31a905ecb1cc9b98dd1218798e253ee0b3ebbd3
Trevor Slaton <trevor.slaton@gmail.com>
2025-07-27 08:36:40 -0500

    reorder Property details subpanels

--- custom/Extension/modules/Properties/Ext/Layoutdefs/properties_subpanels.php
+++ custom/Extension/modules/Properties/Ext/Layoutdefs/properties_subpanels.php
@@ -3,7 +3,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-// Related Contacts subpanel
+// Related Contacts subpanel (1st position)
 $layout_defs['Properties']['subpanel_setup']['contacts_roles'] = array(
     'order' => 100,
     'module' => 'Contacts',
@@ -23,7 +23,7 @@ $layout_defs['Properties']['subpanel_setup']['contacts_roles'] = array(
     ),
 );
 
-// Related Transactions (Opportunities) subpanel
+// Related Transactions (Opportunities) subpanel (2nd position)
 $layout_defs['Properties']['subpanel_setup']['opportunities'] = array(
     'order' => 200,
     'module' => 'Opportunities',
@@ -43,7 +43,7 @@ $layout_defs['Properties']['subpanel_setup']['opportunities'] = array(
     ),
 );
 
-// Documents & CMAs subpanel
+// Documents & CMAs subpanel (3rd position)
 $layout_defs['Properties']['subpanel_setup']['documents'] = array(
     'order' => 300,
     'module' => 'Documents',
@@ -59,6 +59,71 @@ $layout_defs['Properties']['subpanel_setup']['documents'] = array(
         1 => array(
             'widget_class' => 'SubPanelTopSelectButton',
             'mode' => 'MultiSelect',
+        ),
+    ),
+);
+
+// Activities subpanel (4th position)
+$layout_defs['Properties']['subpanel_setup']['activities'] = array(
+    'order' => 400,
+    'sort_order' => 'desc',
+    'sort_by' => 'date_start',
+    'title_key' => 'LBL_ACTIVITIES_SUBPANEL_TITLE',
+    'type' => 'collection',
+    'subpanel_name' => 'activities',
+    'module' => 'Activities',
+    'top_buttons' => array(
+        0 => array(
+            'widget_class' => 'SubPanelTopCreateButton',
+            'view' => 'Activities',
+        ),
+    ),
+    'collection_list' => array(
+        'meetings' => array(
+            'module' => 'Meetings',
+        ),
+        'tasks' => array(
+            'module' => 'Tasks',
+        ),
+        'calls' => array(
+            'module' => 'Calls',
+        ),
+        'notes' => array(
+            'module' => 'Notes',
+        ),
+    ),
+);
+
+// History subpanel (5th position)
+$layout_defs['Properties']['subpanel_setup']['history'] = array(
+    'order' => 500,
+    'sort_order' => 'desc',
+    'sort_by' => 'date_entered',
+    'title_key' => 'LBL_HISTORY_SUBPANEL_TITLE',
+    'type' => 'collection',
+    'subpanel_name' => 'history',
+    'module' => 'History',
+    'top_buttons' => array(
+        0 => array(
+            'widget_class' => 'SubPanelTopCreateButton',
+            'view' => 'History',
+        ),
+    ),
+    'collection_list' => array(
+        'meetings' => array(
+            'module' => 'Meetings',
+        ),
+        'tasks' => array(
+            'module' => 'Tasks',
+        ),
+        'calls' => array(
+            'module' => 'Calls',
+        ),
+        'notes' => array(
+            'module' => 'Notes',
+        ),
+        'emails' => array(
+            'module' => 'Emails',
         ),
     ),
 );
